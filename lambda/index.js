b49776681e9abd48e188ecf9244c4c65640bc496
God Never Fail <kingmanne@MacBook-Air.local>
2026-04-27 11:51:57 +0100

    Multiplatform

--- lambda/index.js
+++ lambda/index.js
@@ -1,11 +1,9 @@
 exports.handler = async (event) => {
-    console.log("Event received:", JSON.stringify(event, null, 2));
-
     return {
         statusCode: 200,
         body: JSON.stringify({
-            message: "Hello from Lambda",
-            input: event
+            message: "Deployed via CI/CD",
+            env: process.env.ENVIRONMENT
         })
     };
 };
